dc19e7fc6dc2d68ebaf7e1dea36827d4b626e702
cdconrad <cdconrad11@gmail.com>
2016-03-01 07:30:32 -0400

    adding week 5 content

---
+++ week5/fintech/polls/views.py
@@ -0,0 +1,8 @@
+#views for the polls app, as suggested by the Django tutorial
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
